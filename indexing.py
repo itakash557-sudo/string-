4429181a46c72302b0c57d88d1409d7854ef2fdd
AKASH S <itakash557@gmail.com>
2025-08-17 21:49:40 +0530

    Create indexing.py
    this program contains indexing in string concept

---
+++ indexing.py
@@ -0,0 +1,10 @@
+name = "akash it"
+
+# Indexing:accessing characters using position
+print(name[0])     # Output: a   (first character)
+print(name[2])     # Output: a   (third character, index starts from 0)
+print(name[-1])    # Output: t   (last character using negative index)
+print(name[0:5])   # Output: akash   (slicing from index 0 to 4)
+print(name[:5])    # Output: akash   (same as above, start is default 0)
+print(name[6:])    # Output: it      (from index 6 till end)
+print(name[::-1])  # Output: ti hsaka   (string reversed)
